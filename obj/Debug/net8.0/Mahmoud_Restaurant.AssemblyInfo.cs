3b7f60c5e6efcc70efe4eb8352dee7f107e9b015
Mahmoud Ouf <mahmoudad707@gmail.com>
2024-12-12 18:26:56 +0700

    initial_commit

--- obj/Debug/net8.0/Mahmoud_Restaurant.AssemblyInfo.cs
+++ obj/Debug/net8.0/Mahmoud_Restaurant.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mahmoud_Restaurant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0139b20b52c9d272b224bc1ae9ad4a17107aff2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a4de90b99634c590fddf146cf85caffdf140870")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mahmoud_Restaurant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mahmoud_Restaurant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
